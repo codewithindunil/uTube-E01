593b0f492655627215217d23214d9e12c753a6c7
codewithindunil <codewithindunil@gmail.com>
2020-04-08 19:57:47 +0530

    led

---
+++ led/led.ino
@@ -0,0 +1,16 @@
+void setup() {
+  // put your setup code here, to run once:
+  //pin no =7
+  pinMode(7,Output);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(7,HIGH);
+  delay(1000);
+  digitalWrite(7,LOW);
+  delay(1000);
+  
+
+}
